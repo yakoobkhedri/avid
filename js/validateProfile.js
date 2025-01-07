1a6afc30016d37621c942c4a8c939d3191135f03
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-01-07 20:06:39 +0330

    commit message

--- js/validateProfile.js
+++ js/validateProfile.js
@@ -16,7 +16,15 @@ function validateForm() {
     const codemeliRegex = /^\d{10}$/;
     const sajamCodeRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8}$/;
     const shabaCodeRegex = /^IR\d{23}$/;
-
+    
+    firstname.value ? document.querySelector('.firstSuccess').classList.add('active') : document.querySelector('.firstSuccess').classList.remove('active') ;
+    lastname.value ? document.querySelector('.lastSuccess').classList.add('active') : document.querySelector('.lastSuccess').classList.remove('active') ;
+    phone.value && phoneNumberRegex.test(phone.value) ? document.querySelector('.phoneSuccess').classList.add('active') : document.querySelector('.phoneSuccess').classList.remove('active') ;
+    email.value && emailRegex.test(email.value) ? document.querySelector('.emailSuccess').classList.add('active') : document.querySelector('.emailSuccess').classList.remove('active') ;
+    codemeli.value && codemeliRegex.test(codemeli.value) ? document.querySelector('.codemeliSuccess').classList.add('active') : document.querySelector('.codemeliSuccess').classList.remove('active') ;
+    sajamcode.value && sajamCodeRegex.test(sajamcode.value) ? document.querySelector('.sajamcodeSuccess').classList.add('active') : document.querySelector('.sajamcodeSuccess').classList.remove('active') ;
+    shabacode.value && shabaCodeRegex.test(shabacode.value) ? document.querySelector('.shabacodeSuccess').classList.add('active') : document.querySelector('.shabacodeSuccess').classList.remove('active') ;
+    
     if (firstname.value && lastname.value && phone.value && phoneNumberRegex.test(phone.value) && email.value && emailRegex.test(email.value) && codemeli.value && codemeliRegex.test(codemeli.value) && sajamcode.value && sajamCodeRegex.test(sajamcode.value) && shabacode.value && shabaCodeRegex.test(shabacode.value)) {
         statusText.textContent = 'آماده سرمایه گذاری';
         statusText.style.color = '#039855';
