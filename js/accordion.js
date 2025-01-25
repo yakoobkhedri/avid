2a7793a73482d1535e466e45c9113217a089b2a0
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-01-25 17:27:55 +0330

    commit message

--- js/accordion.js
+++ js/accordion.js
@@ -3,9 +3,11 @@
 
 let accordionBtn = Array.from(document.getElementsByClassName('accordionBtn3'));
 let accordionItem = Array.from(document.querySelectorAll('.accordionContent3 > div'));
+let accordionContent3 = Array.from(document.getElementsByClassName('accordionContent3'));
 
 accordionBtn.forEach((item) => {
   item.addEventListener('click', function () {
+    accordionContent3.forEach((items)=>{items.classList.remove('active')});
     item.classList.toggle('active');
     item.nextElementSibling.classList.toggle('active');
   })
